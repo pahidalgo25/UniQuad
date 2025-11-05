f7b7ec3832f94a79a7c8111dd717f50fbdfb589d
PaoloH <paolohidalgo@csus.edu>
2025-11-04 18:21:57 -0800

    Change main.jsx

--- mern-stack/frontend/src/main.jsx
+++ mern-stack/frontend/src/main.jsx
@@ -7,9 +7,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
   </StrictMode>
 )
